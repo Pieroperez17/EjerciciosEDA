4fec5dd252cfcc7d1c9faa2ba2aa8cd93139c935
Pieroperez17 <sppgcorreoinfo@gmail.com>
2025-06-26 17:05:40 -0500

    Pregunta1 realizada

---
+++ Problema1/Nodo.java
@@ -0,0 +1,13 @@
+public class Nodo {
+    int clave;
+    Nodo siguiente;
+
+    public Nodo(int clave) {
+        this.clave = clave;
+        this.siguiente = null;
+    }
+
+    public int GetClave() {
+        return clave;
+    }
+}
---
+++ Problema1/Principal.java
@@ -0,0 +1,57 @@
+
+public class Principal {
+    public static void main(String[] args) {
+        ListaEnlazada listaentrada = new ListaEnlazada();
+
+        listaentrada.insertarFinal(1);
+        listaentrada.insertarFinal(2);
+        listaentrada.insertarFinal(3);
+        listaentrada.insertarFinal(4);
+        listaentrada.insertarFinal(5);
+
+        System.out.println("Entrada");
+        listaentrada.mostrar();
+
+        System.out.println("Salida");
+        unirValorNombre(listaentrada, 3);
+    }
+
+    
+    private static void unirValorNombre(ListaEnlazada input,int k) {
+        if (input.estaVacia() || k <= 0 || k > input.obtenerTamanio()) {
+            System.out.println("Lista vacía o valor de k inválido");
+            return;
+        }
+
+        ListaEnlazada ListaImput = input;
+        ListaEnlazada temp1 = new ListaEnlazada();
+        ListaEnlazada temp2 = new ListaEnlazada();
+        int size = input.obtenerTamanio();
+
+
+        for(int i=0; i<size;i++){
+            if (i < k) {
+                temp1.insertarFinal(ListaImput.getHead().GetClave());
+                ListaImput.eliminarInicio();
+            } else {
+                temp2.insertarFinal(ListaImput.getHead().GetClave());
+                ListaImput.eliminarInicio();
+            }
+        }
+        temp1.revertir();
+        temp2.revertir();
+        
+        for (int index = 0; index < size; index++) {
+            if (index < k) {
+                System.out.print(temp1.getHead().GetClave());
+                temp1.eliminarInicio();
+            } else {
+                System.out.print(temp2.getHead().GetClave());
+                temp2.eliminarInicio();
+            }
+            System.out.print(" --> ");
+        }
+        System.out.print("null");
+    }
+
+}
